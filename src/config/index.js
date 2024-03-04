45c2b957cfbf0300b21a8a01caf6ccaa6b49bf1d
Nitin Mittal <nitinmittal44066@gmail.com>
2024-03-05 01:12:21 +0400

    add: env example

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,9 @@ config();
 
 export default {
   db: {
-    url: process.env.MONGO_CONNECTION,
+    url: process.env.MONGO_CONNECTION || 'mongodb://localhost:27017/avail',
   },
   NODE_ENV: process.env.NODE_ENV || 'production',
-  BRIDGE_API: process.env.BRIDGE_API || 'https://bridge-api.sandbox.avail.tools'
+  BRIDGE_API: process.env.BRIDGE_API || 'https://bridge-api.sandbox.avail.tools',
+  SUBGRAPH_URL: process.env.SUBGRAPH_URL
 };
