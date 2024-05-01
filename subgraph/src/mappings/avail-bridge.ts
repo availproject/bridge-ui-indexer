a969f3666c320da36e42ed4f3a07dc60eced9812
vibhurajeev <vibhurajeev@gmail.com>
2024-05-01 17:40:32 +0400

    feat(subgraph): Amount in send message

--- subgraph/src/mappings/avail-bridge.ts
+++ subgraph/src/mappings/avail-bridge.ts
@@ -1,3 +1,4 @@
+import { BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { MessageSent, MessageReceived } from '../../generated/AvailBridge/AvailBridge'
 import { SendMessage, ReceiveMessage } from '../../generated/schema'
 
@@ -8,6 +9,15 @@ export function handleMessageSent(event: MessageSent): void {
   let entity = SendMessage.load(id)
   if (entity == null) {
     entity = new SendMessage(id)
+  }
+  const transferEvents = event.receipt.logs.filter(log => log.logIndex == event.logIndex.plus(new BigInt(1)));
+
+  if (transferEvents.length) {
+    const transferEvent = transferEvents[0];
+
+    if (transferEvent.topics[1] == event.params.from && transferEvent.topics[2] == Bytes.fromHexString("0x0000000000000000000000000000000000000000")) {
+      entity.amount = BigInt.fromByteArray(transferEvent.data);
+    }
   }
 
   entity.from = event.params.from
