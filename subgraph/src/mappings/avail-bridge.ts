c026981621065e604f56c033782b4d9f8c2b717d
vibhurajeev <vibhurajeev@gmail.com>
2024-05-01 18:06:58 +0400

    fix(subgraph): check transfer event address

--- subgraph/src/mappings/avail-bridge.ts
+++ subgraph/src/mappings/avail-bridge.ts
@@ -1,7 +1,6 @@
-import { BigInt, Bytes } from '@graphprotocol/graph-ts'
+import { Address, BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { MessageSent, MessageReceived } from '../../generated/AvailBridge/AvailBridge'
 import { SendMessage, ReceiveMessage } from '../../generated/schema'
-
 
 export function handleMessageSent(event: MessageSent): void {
   let id = 'message-sent-' + event.params.messageId.toString()
@@ -10,12 +9,17 @@ export function handleMessageSent(event: MessageSent): void {
   if (entity == null) {
     entity = new SendMessage(id)
   }
-  const transferEvents = event.receipt.logs.filter(log => log.logIndex == event.logIndex.plus(new BigInt(1)));
+  const transferEvents = event.receipt.logs.filter((log) => log.logIndex == event.logIndex.plus(new BigInt(1)));
 
   if (transferEvents.length) {
     const transferEvent = transferEvents[0];
+    const availAddress = event.address == Address.fromHexString("0x967F7DdC4ec508462231849AE81eeaa68Ad01389") ?
+      Address.fromHexString("0xb1C3Cb9b5e598d4E95a85870e7812B99f350982d") :
+      //TODO: Change below to actual Avail address
+      Address.fromHexString("0xb1C3Cb9b5e598d4E95a85870e7812B99f350982d");
 
-    if (transferEvent.topics[1] == event.params.from && transferEvent.topics[2] == Bytes.fromHexString("0x0000000000000000000000000000000000000000")) {
+
+    if (transferEvent.address == availAddress && transferEvent.topics[1] == event.params.from && transferEvent.topics[2] == Bytes.fromHexString("0x0000000000000000000000000000000000000000")) {
       entity.amount = BigInt.fromByteArray(transferEvent.data);
     }
   }
