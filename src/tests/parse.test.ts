0ac82576c8c8680fffa15b9ac647ef18eef78c3c
Jeremy <jeremias@availproject.org>
2024-08-07 00:09:39 -0300

    feat: Add 2 test cases using native Node.js Assert API

---
+++ src/tests/parse.test.ts
@@ -0,0 +1,22 @@
+import assert from "assert/strict"
+import { BigNumber } from "bignumber.js";
+import parseAmount from "../helpers/parser.js";
+
+function testNormalBigAmount(){
+    const rawFungibleTokenAmount = "0x00000000000000000de0b6b3a7640000";
+    const bigNumberString = BigNumber(rawFungibleTokenAmount, 16).toFixed(0);
+    const amountPrettified = parseAmount(bigNumberString)
+    const expectedParsedAmount = "1.0"
+    assert.deepStrictEqual(amountPrettified,expectedParsedAmount)
+}
+
+function testScientificBigAmount() {
+    const rawFungibleTokenAmount = "0x000000000000182a697403a1b0cf0ad2";
+    const bigNumberString = BigNumber(rawFungibleTokenAmount, 16).toFixed(0);
+    const amountPrettified = parseAmount(bigNumberString)
+    const expectedParsedAmount = "114119.157542431558142674"
+    assert.deepStrictEqual(amountPrettified,expectedParsedAmount)
+}
+
+testNormalBigAmount()
+testScientificBigAmount()
