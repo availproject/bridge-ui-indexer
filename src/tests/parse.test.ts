8aaf6eeba9d7d7ea05b0ea1baeff6a2ebd65feca
Jeremy <jeremias@availproject.org>
2024-08-07 00:52:00 -0300

    chore: Remove unused attribute

--- src/tests/parse.test.ts
+++ src/tests/parse.test.ts
@@ -6,7 +6,7 @@ import parseAmount from "../helpers/parser.js";
 describe("Test tx amount parse", () => {
   it("Should parse rawFungibleTokenAmount on 18 digit BigNumber value", () => {
     const rawFungibleTokenAmount = "0x00000000000000000de0b6b3a7640000";
-    const bigNumberString = BigNumber(rawFungibleTokenAmount, 16).toFixed(0);
+    const bigNumberString = BigNumber(rawFungibleTokenAmount, 16).toFixed();
     const amountPrettified = parseAmount(bigNumberString);
     const expectedParsedAmount = "1.0";
     assert.deepStrictEqual(amountPrettified, expectedParsedAmount);
@@ -14,7 +14,7 @@ describe("Test tx amount parse", () => {
 
   it("Should parse rawFungibleTokenAmount on 24 digit scientific BigNumber value", () => {
     const rawFungibleTokenAmount = "0x000000000000182a697403a1b0cf0ad2";
-    const bigNumberString = BigNumber(rawFungibleTokenAmount, 16).toFixed(0);
+    const bigNumberString = BigNumber(rawFungibleTokenAmount, 16).toFixed();
     const amountPrettified = parseAmount(bigNumberString);
     const expectedParsedAmount = "114119.157542431558142674";
     assert.deepStrictEqual(amountPrettified, expectedParsedAmount);
